0c95e1d0f35db2f91f9aee96ac0a600e799bddc0
041 <041@bupt.edu.cn>
2024-02-20 18:48:45 +0800

    update .gitignore

--- expr.sh
+++ expr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# following codes are assumed to be executed undet conda env "LERS*"
+# following codes are assumed to be executed under conda env "LERS*"
 
 ### different batch_size_by_HADMID ###
 # python main.py --use_gpu --train --batch_size_by_HADMID=512
@@ -7,43 +7,47 @@
 # python main.py --use_gpu --train --batch_size_by_HADMID=128
 
 ### different gnn_type ###
-# python main.py --use_gpu --train --gnn_type=GINEConv
-# python main.py --use_gpu --train --gnn_type=GENConv
-# python main.py --use_gpu --train --gnn_type=GATConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GINEConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GATConv
 
-# python main.py --use_gpu --train --gnn_type=GINEConv
-# python main.py --use_gpu --train --gnn_type=GENConv
-# python main.py --use_gpu --train --gnn_type=GATConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GINEConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GATConv
 
-# python main.py --use_gpu --train --gnn_type=GINEConv
-# python main.py --use_gpu --train --gnn_type=GENConv
-# python main.py --use_gpu --train --gnn_type=GATConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GINEConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GATConv
 
-# python main.py --use_gpu --train --gnn_type=GINEConv
-# python main.py --use_gpu --train --gnn_type=GENConv
-# python main.py --use_gpu --train --gnn_type=GATConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GINEConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GATConv
 
-# python main.py --use_gpu --train --gnn_type=GINEConv
-# python main.py --use_gpu --train --gnn_type=GENConv
-# python main.py --use_gpu --train --gnn_type=GATConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GINEConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv
+#python main.py --use_gpu --num_gpu=1 --train --gnn_type=GATConv
 
-### validating ###
-# python main.py --use_gpu --val --val_num=1 --gnn_type=GINEConv --val_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=7.8410"
-# python main.py --use_gpu --val --val_num=1 --gnn_type=GATConv  --val_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=5.1474"
-# python main.py --use_gpu --val --val_num=1 --gnn_type=GENConv  --val_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.5760"
+# python main.py --use_gpu --num_gpu=1 --train --gnn_type=GENConv --batch_size_by_HADMID=256
 
-python main.py --use_gpu --val --val_num=2 --gnn_type=GINEConv --val_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=7.0852"
-python main.py --use_gpu --val --val_num=2 --gnn_type=GATConv  --val_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.9369"
-python main.py --use_gpu --val --val_num=2 --gnn_type=GENConv  --val_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.5982"
+### testing ###
+#python main.py --use_gpu --test --test_num=1 --gnn_type=GINEConv --test_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=5.7964"
+#python main.py --use_gpu --test --test_num=1 --gnn_type=GATConv  --test_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.5010"
+python main.py --use_gpu --test --test_num=1 --gnn_type=GENConv  --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.1443"
+#
+#python main.py --use_gpu --test --test_num=2 --gnn_type=GINEConv --test_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=5.8608"
+#python main.py --use_gpu --test --test_num=2 --gnn_type=GATConv  --test_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.5607"
+#python main.py --use_gpu --test --test_num=2 --gnn_type=GENConv  --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.2983"
+#
+#python main.py --use_gpu --test --test_num=3 --gnn_type=GINEConv --test_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=5.7413"
+#python main.py --use_gpu --test --test_num=3 --gnn_type=GATConv  --test_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.6074"
+#python main.py --use_gpu --test --test_num=3 --gnn_type=GENConv  --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.2756"
+#
+#python main.py --use_gpu --test --test_num=4 --gnn_type=GINEConv --test_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=5.9453"
+#python main.py --use_gpu --test --test_num=4 --gnn_type=GATConv  --test_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.4467"
+#python main.py --use_gpu --test --test_num=4 --gnn_type=GENConv  --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.2079"
+#
+#python main.py --use_gpu --test --test_num=5 --gnn_type=GINEConv --test_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=5.2503"
+#python main.py --use_gpu --test --test_num=5 --gnn_type=GATConv  --test_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.3887"
+#python main.py --use_gpu --test --test_num=5 --gnn_type=GENConv  --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.2225"
 
-python main.py --use_gpu --val --val_num=3 --gnn_type=GINEConv --val_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=7.0301"
-python main.py --use_gpu --val --val_num=3 --gnn_type=GATConv  --val_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.9566"
-python main.py --use_gpu --val --val_num=3 --gnn_type=GENConv  --val_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.6829"
-
-python main.py --use_gpu --val --val_num=4 --gnn_type=GINEConv --val_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=9.2426"
-python main.py --use_gpu --val --val_num=4 --gnn_type=GATConv  --val_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.9479"
-python main.py --use_gpu --val --val_num=4 --gnn_type=GENConv  --val_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.5144"
-
-python main.py --use_gpu --val --val_num=5 --gnn_type=GINEConv --val_model_state_dict="task=MIX_gnn_type=GINEConv_batch_size_by_HADMID=128_loss=7.4923"
-python main.py --use_gpu --val --val_num=5 --gnn_type=GATConv  --val_model_state_dict="task=MIX_gnn_type=GATConv_batch_size_by_HADMID=128_loss=4.9054"
-python main.py --use_gpu --val --val_num=5 --gnn_type=GENConv  --val_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=128_loss=3.5710"
+#python main.py --use_gpu --num_gpu=1 --test --gnn_type=GENConv --batch_size_by_HADMID=256 --test_model_state_dict="task=MIX_gnn_type=GENConv_batch_size_by_HADMID=256_loss=4.0512"
