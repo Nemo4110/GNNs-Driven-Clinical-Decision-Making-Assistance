e8836249d9139fd476694624af2f92b382f23698
Nemo4110 <381151237@qq.com>
2024-08-02 22:17:19 +0800

    tested DeepFM

--- demo.py
+++ demo.py
@@ -9,16 +9,28 @@ if __name__ == '__main__':
     sources_dfs = SourceDataFrames(r"data\mimic-iii-clinical-database-1.4")
     dataset = SingleItemType(sources_dfs, "val", "labitem")
 
+    """DSSM"""
+    # config = {
+    #     "mlp_hidden_size": [128, 64, 32],
+    #     "dropout_prob": 0.1,
+    #     "embedding_size": 32,
+    #     "double_tower": True,
+    #     "device": torch.device('cpu'),
+    #     "LABEL_FIELD": "label",
+    #     "numerical_features": dataset.fields(ftype=[FeatureType.FLOAT])
+    # }
+    # net = context_aware_recommender.DSSM(config, dataset)
+
+    """DeepFM"""
     config = {
-        "mlp_hidden_size": [256, 64, 32],
+        "mlp_hidden_size": [128, 64, 32],
         "dropout_prob": 0.1,
         "embedding_size": 32,
-        "double_tower": True,
         "device": torch.device('cpu'),
         "LABEL_FIELD": "label",
         "numerical_features": dataset.fields(ftype=[FeatureType.FLOAT])
     }
+    net = context_aware_recommender.DeepFM(config, dataset)
 
-    net = context_aware_recommender.DSSM(config, dataset)
     loss = net.calculate_loss(dataset[2])
     print(loss)
