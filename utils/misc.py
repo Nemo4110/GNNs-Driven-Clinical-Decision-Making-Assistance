668e915902c25506ffa73d6cb5dc0ef3e0561f6a
Nemo4110 <381151237@qq.com>
2024-08-02 17:33:33 +0800

    1. define AbstractRecommender 2. init.py 3. define more useful model layers

--- utils/misc.py
+++ utils/misc.py
@@ -47,6 +47,20 @@ def get_latest_model_ckpt(folder_path):
     return os.path.basename(latest_file)
 
 
+def set_color(log, color, highlight=True):
+    color_set = ["black", "red", "green", "yellow", "blue", "pink", "cyan", "white"]
+    try:
+        index = color_set.index(color)
+    except:
+        index = len(color_set) - 1
+    prev_log = "\033["
+    if highlight:
+        prev_log += "1;3"
+    else:
+        prev_log += "0;3"
+    prev_log += str(index) + "m"
+    return prev_log + log + "\033[0m"
+
+
 if __name__ == "__main__":
-    latest_model_ckpt = get_latest_model_ckpt(r"..\model\hub")
-    print(latest_model_ckpt)
+    print(set_color("\nTrainable parameters", "blue"))
