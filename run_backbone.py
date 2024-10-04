7517be662a311b61df44691e0e838e1cd4f76ae2
Nemo4110 <381151237@qq.com>
2024-10-04 16:00:39 +0800

    restart multi-epochs training

--- run_backbone.py
+++ run_backbone.py
@@ -38,7 +38,8 @@ if __name__ == '__main__':
     parser.add_argument("--goal", default="drug", help="the goal of the recommended task, in ['drug', 'labitem']")
     parser.add_argument("--is_gnn_only", action="store_true", default=False, help="whether to only use GNN")
     parser.add_argument("--train", action="store_true", default=False)
-    parser.add_argument("--patience", type=int, default=3)
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--patience", type=int, default=5)
     parser.add_argument("--test", action="store_true", default=False)
     parser.add_argument("--notes", default=None, help="experiment description and running args")
     parser.add_argument("--model_ckpt", default=None, help="the .pt filename where stores the state_dict of model")
@@ -67,44 +68,47 @@ if __name__ == '__main__':
         valid_dataset = OneAdmOneHG(sources_dfs, "val")
         optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
         early_stopper = EarlyStopper(args.patience, False)
-        if args.use_gpu:
-            torch.cuda.empty_cache()
-        train_metric = d2l.Accumulator(2)  # train loss, iter num
-        model.train()
-        train_loop = tqdm(enumerate(train_dataset), leave=False, ncols=80, total=len(train_dataset))
-        for i, hg in train_loop:
-            hg = hg.to(device)
-            logits, labels = model(hg)
-            loss = BackBoneV2.get_loss(logits, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-            optimizer.step()
+        for epoch in range(args.epochs):
+            if args.use_gpu:
+                torch.cuda.empty_cache()
 
-            with torch.no_grad():
-                train_metric.add(loss.detach().item(), 1)
-                train_loop.set_postfix_str(f'train loss: {loss.item():.4f}')
+            # TRAIN STAGE
+            train_metric = d2l.Accumulator(2)  # train loss, iter num
+            model.train()
+            train_loop = tqdm(enumerate(train_dataset), leave=False, ncols=80, total=len(train_dataset))
+            for i, hg in train_loop:
+                hg = hg.to(device)
+                logits, labels = model(hg)
+                loss = BackBoneV2.get_loss(logits, labels)
+                optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                optimizer.step()
 
-                # 每遍历完训练集的10%或最后一个，在验证集上计算下loss
-                if (i > 0 and i % (len(train_dataset) // 10) == 0) or i == (len(train_dataset) - 1):
-                    model.eval()
-                    valid_metric = d2l.Accumulator(2)
-                    for hg in valid_dataset:
-                        hg = hg.to(device)
-                        logits, labels = model(hg)
-                        validloss = BackBoneV2.get_loss(logits, labels)
-                        valid_metric.add(validloss.item(), 1)
-                        train_loop.set_postfix_str(f'valid loss: {validloss.item():.4f}')
-                    valid_loss = valid_metric[0] / valid_metric[1]  # 计算验证集上的平均loss
-                    model.train()  # 验证完了，返回训练模式
+                # VALID STAGE
+                with torch.no_grad():
+                    train_metric.add(loss.detach().item(), 1)
+                    train_loop.set_postfix_str(f'train loss: {loss.item():.4f}')
 
-                    early_stopper(valid_loss, model)
-                    if early_stopper.is_stop or i == (len(train_dataset) - 1):  # 早停了或到训练集遍历到最后了
-                        model_name = f"loss_{early_stopper.best_score:.4f}_{model.__class__.__name__}_goal_{args.goal}.pt"
-                        early_stopper.save_checkpoint(args.path_dir_model_hub, model_name, args.notes)  # 保存valid_loss最低的模型参数检查点
-                        break
+                    if i > 0 and i % (len(train_dataset) // 10) == 0:  # 每遍历完训练集的10%
+                        model.eval()
+                        valid_metric = d2l.Accumulator(2)
+                        for hg in valid_dataset:
+                            hg = hg.to(device)
+                            logits, labels = model(hg)
+                            validloss = BackBoneV2.get_loss(logits, labels)
+                            valid_metric.add(validloss.item(), 1)
+                            train_loop.set_postfix_str(f'valid loss: {validloss.item():.4f}')
+                        valid_loss = valid_metric[0] / valid_metric[1]  # 计算验证集上的平均loss
+                        model.train()  # 验证完了，返回训练模式
+                        early_stopper(valid_loss, model)
+                        if early_stopper.is_stop: break
 
-        print(f"avg. train loss: {train_metric[0] / train_metric[1]:.4f}")
+            print(f"avg. train loss of epoch #{epoch:02}: {train_metric[0] / train_metric[1]:.4f}")
+            if early_stopper.is_stop: break
+
+        model_name = f"loss_{early_stopper.best_score:.4f}_{model.__class__.__name__}_goal_{args.goal}.pt"
+        early_stopper.save_checkpoint(args.path_dir_model_hub, model_name, args.notes)  # 保存valid_loss最低的模型参数检查点
 
     if args.test:
         test_dataset = OneAdmOneHG(sources_dfs, "test")
